745661652227a4724fca0056a0d9e24255972945
puzhelev <puzhelev@gmail.com>
2023-06-01 13:57:06 +0300

    UI action bar, add logo there

--- app/src/main/java/com/example/realestate/MainActivity.java
+++ app/src/main/java/com/example/realestate/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.realestate;
 
 import android.view.Menu;
 import android.view.View;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setLogo(R.drawable.logo);
+        actionBar.setDisplayUseLogoEnabled(true);
+        actionBar.setTitle(R.string.company_name);
     }
     public void onClickRegion(View view)
     {}
