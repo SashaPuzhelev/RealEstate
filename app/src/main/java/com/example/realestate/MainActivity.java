9f7cfe79da1076a1d43ac06999c342cac5dfa620
puzhelev <puzhelev@gmail.com>
2023-06-01 15:19:29 +0300

    add UI login and navigate with login and main home activity

--- app/src/main/java/com/example/realestate/MainActivity.java
+++ app/src/main/java/com/example/realestate/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.realestate;
 
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.realestate.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,6 +31,18 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int idItem = item.getItemId();
+        if (idItem == R.id.enter)
+        {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
 
 
 }
